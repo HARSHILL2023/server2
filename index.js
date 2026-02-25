aeb6ea4f88f670391c1b30048bcb9bee864913ba
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-25 11:59:04 +0530

    changed

--- index.js
+++ index.js
@@ -1,40 +1,63 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const users = [
-  { id: 1, name: "Arjun", role: "student" },
-  { id: 2, name: "Priyesha", role: "mentor" }
-];
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/codingGita')
+.then(()=>console.log("MongoDB Connection Successfully"))
+.catch(()=> console.log("MongoDB Connection Failed"));
 
 
+const studentSchema = new mongoose.Schema({
+    name: String,
+    branch: String,
+    cgpa: Number
+});
 
+
+const Student = mongoose.model("Student", studentSchema);
+
+app.post("/students", async (req, res) => {
+
+    try {
+
+        const data = new Student(req.body);
+
+        await data.save();
+
+        res.status(201).json(data);
+
+    }
+    catch (error) {
+
+        res.status(400).json({ error: error.message });
+
+    }
+
+});
+
+ 
+app.get("/students", async (req, res) => {
+
+    const data = await Student.find({});
+
+    res.status(200).json(data);
+
+});
+
+
+// Home Route
 app.get("/", (req, res) => {
-  res.send("Express server is running");
-});
 
-app.get("/users", (req, res) => {
-  res.status(200).json(users);
-});
+  res.send("Student server is running...");
 
-app.get("/users/:test",(one,two) =>{
-    console.log(one.params);
-    two.status(200).json(users);
-})
-
-
-app.get("/users/:id", (req, res) => {
-  const userId = Number(req.params.id);
-  const user = users.find(u => u.id === userId);
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  res.status(200).json(user);
 });
 
 
 app.listen(3000, () => {
+
   console.log("Server started on port 3000");
+
 });
